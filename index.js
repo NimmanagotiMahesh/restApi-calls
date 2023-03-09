45c237560f3de878b7a084764bb76e93370827e0
NimmanagotiMahesh <126644235+NimmanagotiMahesh@users.noreply.github.com>
2023-03-09 20:56:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Post />
+    <Get />
+//  <Post />
   </React.StrictMode>
 );
 
